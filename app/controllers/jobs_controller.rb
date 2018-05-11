4c2fbbd469006a7e651bbcfa2e3c44e24f1e8eed
martinezk <misskristinamartinez@gmail.com>
2018-05-11 10:43:04 -0400

    Finish styling

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,6 +7,7 @@ class JobsController < ApplicationController
     else
       @category_id = Category.find_by(name: params[:category]).id
       @jobs = Job.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def show
